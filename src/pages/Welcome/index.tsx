d6927994092503e8b1b71b28f9ecf5e8a6d29d25
pedroW7439 <pedrowasem@hotmail.com>
2023-04-05 20:07:12 -0300

    commit inicial

---
+++ src/pages/Welcome/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import reactLogo from "/assets/react.svg";
+import viteLogo from "/assets/vite.svg";
+
+const Welcome: React.FC = () => {
+    return (
+        <div className="App">
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://reactjs.org" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Template app react com Typescript e React Router Dom</h1>
+        </div>
+    );
+};
+
+export default Welcome;
